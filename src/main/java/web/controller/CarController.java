fd9bf6df444ebb6ec79127ef642f345f2a45f58a
Kamil <1505kamil@gmail.com>
2025-03-20 22:24:41 +0500

    Реализована страница cars

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,24 +1,35 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class CarsController {
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("BMW", 3, "Black"));
-        cars.add(new Car("Audi", 2, "White"));
-        cars.add(new Car("Lada", 1, "Red"));
-        cars.add(new Car("Opel", 4, "Blue"));
-        cars.add(new Car("Mercedes", 5, "Green"));
-        return "index";
+    public String getCars(
+            @RequestParam(value = "count", required = false) Integer count,
+            ModelMap model) {
+        if (count == null || count >= 5) {
+            model.addAttribute("cars", carService.getCars(5));
+        } else {
+            model.addAttribute("cars", carService.getCars(count));
+        }
+        return "cars";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,5 +1,28 @@
 package web.service;
 
-public class CarService {
+import org.springframework.stereotype.Service;
+import web.model.Car;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private final List<Car> cars;
+
+    public CarServiceImpl() {
+        cars = new ArrayList<>();
+        cars.add(new Car("BMW", 3, "Black"));
+        cars.add(new Car("Audi", 2, "White"));
+        cars.add(new Car("Lada", 1, "Red"));
+        cars.add(new Car("Opel", 4, "Blue"));
+        cars.add(new Car("Mercedes", 5, "Green"));
+    }
+
+
+    @Override
+    public List<Car> getCars(int count) {
+        return cars.subList(0, Math.min(count, cars.size()));
+    }
 }
